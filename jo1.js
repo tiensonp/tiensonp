ccc605543fe59da5f77e1a38201ef7c539d0af6d
tiensonp <94881578+tiensonp@users.noreply.github.com>
2021-11-23 10:05:24 +0700

    Update and rename jo.js to jo1.js

--- jo1.js
+++ jo1.js
@@ -1,6 +1,4 @@
 function duckduckgo1(e) {
     var data =JSON.stringify(e)
     document.getElementById("tags").innerHTML = 'adsog'+data
-    //var keyword1 = e[0].phrase
-    //document.getElementById("demo1").innerHTML = keyword1    
 }
