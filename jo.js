c9f0f6e9b138a189f4f3d4fbdd673635260aafc1
tiensonp <94881578+tiensonp@users.noreply.github.com>
2021-11-23 10:03:23 +0700

    Update jo.js

--- jo.js
+++ jo.js
@@ -1,1 +1,6 @@
-sdgsdgs
+function duckduckgo1(e) {
+    var data =JSON.stringify(e)
+    document.getElementById("tags").innerHTML = 'adsog'+data
+    //var keyword1 = e[0].phrase
+    //document.getElementById("demo1").innerHTML = keyword1    
+}
